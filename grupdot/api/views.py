c42590280e949056f47a09c61931f2ed2ac76f60
nicolasherrerac <nicolasherrerac20@gmail.com>
2021-08-04 21:44:44 -0500

    Fix nombres

--- grupdot/api/views.py
+++ grupdot/api/views.py
@@ -14,13 +14,13 @@ class CotizacionView(View):
         """Funcion que consulta la api de prestamos"""
         if monto >= 0 and monto <= 3000000:
             valor = Vf(0.012, monto)
-            impresion = Impresion('María', valor, '1.2%')
+            impresion = Impresion('Maria', valor, '1.2%')
         elif monto > 3000000 and monto <= 5000000:
             valor = Vf(0.015, monto)
             impresion = Impresion('Juan', valor, '1.5%')
         elif monto > 5000000 and monto <= 7500000:
             valor = Vf(0.02, monto)
-            impresion = Impresion('Andrés', valor, '2.0%')
+            impresion = Impresion('Andres', valor, '2.0%')
         elif monto > 7500000:
             datos = {'messenge': "No hay socio disponible"}
             return JsonResponse(datos)
